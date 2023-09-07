7dd79ba0734314a12a3be3452ab5f17537bc5382
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-07 15:40:30 +0800

    Initial Sample

---
+++ FirstProject/src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author CollegeComlab-10
+ */
+public class FirstProject {
+//this is a comment
+    public static void main(String[] args) 
+    {
+//        System.out.println("Hello World");
+//        System.out.print("Hello World");
+//        System.out.println("Hello Java Programming");
+////        System.out.println("Welcome to Intermediate Programming!");
+//        System.out.println("Welcome to Intermediate Programming!");
+//        System.out.println("3+1");
+//        System.out.println(3*3);
+//        System.out.println(300);
+
+/* VARIABLES
+//   
+        Syntax: 
+        type variableName = value
+        
+   ---- The general rules for naming variables are:
+
+        -Names can contain letters, digits, underscores, and dollar signs
+        -Names must begin with a letter
+        -Names should start with a lowercase letter and it cannot contain whitespace
+        -Names can also begin with $ and _ (but we will not use it in this tutorial)
+        -Names are case sensitive ("myVar" and "myvar" are different variables)
+        -Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+
+          String is for TEXT
+          Char is for single character 'a' 'A' 'B'
+          int (Integer) is for whole numbers
+          float (Float) is for decimal numbers
+//        String name = "John";
+//        System.out.println(name);
+//        int num = 300;
+//        System.out.println(num);
+//        float myFloatNum = 5.99f;
+//        System.out.println(myFloatNum);
+*/        
+//          String firstName = "Johnny";
+//          char middleInitial = 'J';
+//          String lastName = "Yes Papa!";
+//          System.out.println(firstName + " " + middleInitial+ ". " + lastName);
+//          int x = 100;
+//          int y = 1000;
+//          System.out.println(x+y);
+        
+/*
+        
+  JAVA DATA TYPE
+  Data types are divided into two groups:
+
+Primitive data types - includes byte, 
+        short, int, long, float, double, boolean and char
+Non-primitive data types - such as String, Arrays and Classes          
+*/      
+//          int myNum = 32768;                // Integer (whole number) byte, short, int, long
+//          float myFloatNum = 5.99f;         // Floating point number
+//          float pi = 3.14156310519816f;     // double point number
+//          char myLetter = 'D';              // Character
+//          boolean myBool = true;            // Boolean
+//          String myText = "Hello";          // String      
+//          System.out.println(5>5);
+        
+        
+/* JAVA OPERATORS
+        ARITHMENTIC OPERATORS
+            + - * / % ++ increment  --decrement
+        COMPARISON OPERATORS
+            >  <  >=  <= != ==
+        LOGICAL OPERATORS
+            AND     OR      NOT
+            &&      ||      !
+        
+       int 
+            
+*/
+       System.out.println(!(10 >5));
+        
+
+    }
+      
+            
+}
---
+++ SecondProject/src/main/java/com/mycompany/secondproject/SecondProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.secondproject;
+
+/**
+ *
+ * @author CollegeComlab-10
+ */
+public class SecondProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
